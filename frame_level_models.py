3e3b79b8e8ba3b19d09a25772bab39ad9e1e9138
dnuang <danny.dopa@gmail.com>
2017-05-27 21:36:32 +0000

    frame model #

--- frame_level_models.py
+++ frame_level_models.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 """Contains a collection of models which operate on variable-length sequences.
 """
