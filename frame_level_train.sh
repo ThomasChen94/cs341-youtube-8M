3fe33feefc39046b67a2b084e96b3e585a5d4c0a
dnuang <danny.dopa@gmail.com>
2017-05-30 03:24:10 +0000

    shuffle_learn_layer

--- frame_level_train.sh
+++ frame_level_train.sh
@@ -2,7 +2,7 @@ MODEL_DIR=/home/cs341/model
 python train.py --train_data_pattern='/home/cs341/data/frame_level/train/train*.tfrecord' \
 --frame_features=True --model=RankModel \
 --feature_names="rgb" \
---feature_sizes="1024" --batch_size=64 \
+--feature_sizes="1024" --batch_size=1 \
 --train_dir=$MODEL_DIR/frame_level_rank_model_only_shuffle \
 --base_learning_rate=0.0001
 
